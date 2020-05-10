b6a1b2a98ecd59f53d0c58a24c4f1f234d1a543c
matus <matus123@gmail.com>
2020-05-10 19:38:42 +0100

    Add project files.

---
+++ Space Invaders/Form1.Designer.cs
@@ -0,0 +1,105 @@
+﻿namespace Space_Invaders
+{
+    partial class gameWindow
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.ship = new System.Windows.Forms.PictureBox();
+            this.invaderTemplate = new System.Windows.Forms.PictureBox();
+            this.arrow = new System.Windows.Forms.PictureBox();
+            this.gameTimer = new System.Windows.Forms.Timer(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.ship)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.invaderTemplate)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.arrow)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // ship
+            // 
+            this.ship.Image = global::Space_Invaders.Properties.Resources.bow_ship;
+            this.ship.Location = new System.Drawing.Point(481, 599);
+            this.ship.Name = "ship";
+            this.ship.Size = new System.Drawing.Size(100, 50);
+            this.ship.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.ship.TabIndex = 0;
+            this.ship.TabStop = false;
+            // 
+            // invaderTemplate
+            // 
+            this.invaderTemplate.Image = global::Space_Invaders.Properties.Resources.invader;
+            this.invaderTemplate.Location = new System.Drawing.Point(12, 12);
+            this.invaderTemplate.Name = "invaderTemplate";
+            this.invaderTemplate.Size = new System.Drawing.Size(100, 70);
+            this.invaderTemplate.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.invaderTemplate.TabIndex = 1;
+            this.invaderTemplate.TabStop = false;
+            this.invaderTemplate.Visible = false;
+            // 
+            // arrow
+            // 
+            this.arrow.Image = global::Space_Invaders.Properties.Resources.arrow_black_background;
+            this.arrow.Location = new System.Drawing.Point(522, 563);
+            this.arrow.Name = "arrow";
+            this.arrow.Size = new System.Drawing.Size(16, 30);
+            this.arrow.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.arrow.TabIndex = 2;
+            this.arrow.TabStop = false;
+            this.arrow.Visible = false;
+            // 
+            // gameTimer
+            // 
+            this.gameTimer.Tick += new System.EventHandler(this.gameTimerTick);
+            // 
+            // gameWindow
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Black;
+            this.ClientSize = new System.Drawing.Size(1084, 661);
+            this.Controls.Add(this.arrow);
+            this.Controls.Add(this.invaderTemplate);
+            this.Controls.Add(this.ship);
+            this.Name = "gameWindow";
+            this.Text = "Hawkeye vs Space Invaders!";
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.gameKeyIsDown);
+            this.KeyUp += new System.Windows.Forms.KeyEventHandler(this.gameKeyIsUp);
+            ((System.ComponentModel.ISupportInitialize)(this.ship)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.invaderTemplate)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.arrow)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox ship;
+        private System.Windows.Forms.PictureBox invaderTemplate;
+        private System.Windows.Forms.PictureBox arrow;
+        private System.Windows.Forms.Timer gameTimer;
+    }
+}
+
---
+++ Space Invaders/Form1.cs
@@ -0,0 +1,242 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Windows.Forms.VisualStyles;
+using System.Xml;
+//using Microsoft.Xna.Framework.Graphics;
+using Space_Invaders.Properties;
+
+namespace Space_Invaders
+{
+
+    public partial class gameWindow : Form
+    {
+        int arrowSpeed = 20;
+        int invaderSpeed = 5;
+        List<PictureBox> arrowList;
+        PictureBox[,] invaderArray = new PictureBox[5,5];
+        bool endGameBool = true;
+        bool shooting = false;
+        int invadersDown = 0;
+        sbyte invaderMoveDirection = 1;
+
+        public gameWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void initializeInvaders()
+        {
+            for (int i = 0; i < invaderArray.GetLength(0); i++)
+            {
+                for (int j = 0; j < invaderArray.GetLength(1); j++)
+                {
+                    invaderArray[i, j] = makeInvader(i,j);
+                    this.Controls.Add(this.invaderArray[i, j]);
+                }
+            }
+        }
+        
+        private void startGame()
+        {
+            arrowSpeed = 20;
+            invaderSpeed = 5;
+            initializeInvaders();
+            endGameBool = false;
+            ship.Left = (ClientRectangle.Width / 2) - (ship.Width / 2);
+            gameTimer.Start();
+
+        }
+
+        private void gameKeyIsDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Left)
+            {
+                if (ship.Left > 25)
+                {
+                    ship.Left -= 25;
+                }
+            }
+            if (e.KeyCode == Keys.Right)
+            {
+                if(ship.Left+25<ClientRectangle.Width-ship.Width)
+                ship.Left += 25;
+            }
+            if (e.KeyCode == Keys.Escape)
+            {
+                Application.Exit();
+            }
+            if (e.KeyCode == Keys.Space)
+            {
+                if (endGameBool == true) {
+                    startGame();
+                } else {
+                    if (shooting == false) {
+                        PictureBox newArrow = makeArrow(ship.Left + ship.Width / 2, ship.Top);
+                        if(arrowList is null) {
+                            arrowList = new List<PictureBox>();
+                        }
+                        arrowList.Add(newArrow);
+                        this.Controls.Add(this.arrowList.Last());
+                    }
+                }
+            }
+        }
+
+
+        private void gameTimerTick(object sender, EventArgs e)
+        {
+            if (arrowList is null) { }
+            else
+            {
+                foreach (PictureBox ar in arrowList)
+                {
+                    ar.Top -= arrowSpeed;
+                    for (int x = 0; x < invaderArray.GetLength(0); x++)
+                    {
+                        for (int y = 0; y < invaderArray.GetLength(1); y++)
+                        {
+                            if (invaderArray[x, y].Visible == true && ar.Visible == true)
+                            {
+                                if (ar.Bounds.IntersectsWith(invaderArray[x, y].Bounds))
+                                {
+                                    invaderArray[x, y].Visible = false;
+                                    ar.Visible = false;
+                                    invadersDown++;
+                                }
+
+                            }
+                        }
+
+                    }
+                }
+            }
+            if (invadersDown == invaderArray.Length)
+            {
+                endGame("win");
+            }
+            bool horizontalMovementAllowed = canLastMove(invaderMoveDirection);
+            foreach (PictureBox invader in invaderArray)
+            {
+                if (invader.Visible == true) {
+                    if (invader.Top +invader.Height >= ship.Top) {
+                        endGame("Lose");
+                    }
+                    if (horizontalMovementAllowed == true) {
+                        invader.Left += invaderSpeed * invaderMoveDirection;
+                    } else {
+                        invader.Top += invaderSpeed;
+                    }
+                }
+            }
+            invaderMoveDirection *= horizontalMovementAllowed ? (sbyte)1 : (sbyte)-1;
+            if (invadersDown > 5) {
+                invaderSpeed = 7;
+            } else if (invadersDown > 10) {
+                invaderSpeed = 11;
+            } else if (invadersDown > 15) {
+                invaderSpeed = 15;
+            }
+            
+        }
+
+        private void endGame(string state)
+        {
+            gameTimer.Stop();
+            endGameBool = true;
+            invadersDown = 0;
+            foreach(PictureBox ar in arrowList) {
+                ar.Visible = false;
+            }
+            foreach(PictureBox invader in invaderArray)
+            {
+                invader.Visible = false;
+            }
+            arrowList.Clear();
+
+        if(state == "win") {
+                MessageBox.Show("Congrats, you win!");
+            } else {
+                MessageBox.Show("Boooooooooo! Try again!");
+            }
+        }
+
+        private PictureBox makeInvader(int x, int y)
+        {
+            PictureBox newInvader = new PictureBox();
+            newInvader.Left = 12 + 175 * x;
+            newInvader.Top = 12 + 85 * y;
+            newInvader.Image = Resources.invader;
+            newInvader.Width = 100;
+            newInvader.Height = 70;
+            newInvader.SizeMode = PictureBoxSizeMode.StretchImage;
+            newInvader.Visible = true;
+            newInvader.Enabled = true;
+            newInvader.BringToFront();
+            return newInvader;
+
+        }
+
+        private PictureBox makeArrow(int x, int y)
+        {
+            PictureBox newArrow = new PictureBox();
+            newArrow.Left = x - 8;
+            newArrow.Top = y;
+            newArrow.Image = Resources.arrow_black_background;
+            newArrow.Width = 16;
+            newArrow.Height = 30;
+            newArrow.SizeMode = PictureBoxSizeMode.StretchImage;
+            newArrow.Visible = true;
+            newArrow.Enabled = true;
+            return newArrow;
+        }
+
+        private void gameKeyIsUp(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Space)
+            {
+                shooting = false;
+            }
+        }
+        
+        private bool canLastMove(sbyte direction)
+        {
+            //finding the first and last column that still has invaders
+            int minX = 99, maxX =0, minY=0, maxY=0;
+            for(int x = 0; x< invaderArray.GetLength(0); x++) {
+                for(int y = 0; y < invaderArray.GetLength(1); y++) {
+                    if (invaderArray[x, y].Visible == true) {
+                        if (minX > x) {
+                            minX = x;
+                            minY = y;
+                        }
+                        if (maxX < x)
+                        {
+                            maxX = x;
+                            maxY = y;
+                        }
+                    }
+                }
+            }
+            if (direction > 0) {
+                //check if there's space to move to the right -> Left + direction
+                return invaderArray[maxX,maxY].Left < (ClientRectangle.Width - invaderArray[maxX, maxY].Width)-invaderSpeed;
+            } else { 
+                //check if there's space to move to the left -> Left - direction
+                return invaderArray[minX, minY].Left > invaderSpeed+5;
+            }
+        }
+    
+    }
+
+    
+    
+
+}
